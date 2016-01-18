5809c0ca7af776a224addd1f98d9a433aca18400
John Ferris <john@pixel-whip.com>
2016-01-18 11:53:52 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,140 @@
+var url = require('url');
+var querystring = require('querystring');
+var mapValues = require('lodash/mapValues');
+var union = require('lodash/union');
+var difference = require('lodash/difference');
+
+var urlParams = function() {
+  
+  /**
+   * Converts the values to an array of strings.
+   * @param  {any} value    The input value
+   * @return {array}        An array of values as strings.
+   */
+  function parseValue(value) {
+    switch (typeof(value)) {
+      // Handle just a param and no values.
+      case 'undefined':
+        return true;
+      case 'object':
+        // Handle arrays
+        if (Array.isArray(value)) {
+          return value.map(parseValue); 
+        } 
+        // Ignore non-array objects.
+        else {
+          return undefined;
+        }
+        break;
+      default:
+        // Handle everything else.
+        return value + '';
+    }
+  }
+
+  /**
+   * Format a url object as a string.
+   * @param  {object} urlObj A url object from createUrlObject()
+   * @return {string}        A url string
+   */
+  function stringify(urlObj) {
+    // Make each multi-value array a '+' delemited string.
+    urlObj.query = mapValues(urlObj.query, function(item) {
+      return Array.isArray(item) ? item.join('+') : item;
+    });
+    // Update the search property to match.
+    urlObj.search = unescape(querystring.stringify(urlObj.query));
+
+    return (unescape(url.format(urlObj)));
+  }
+  
+  // Public methods.
+  var pub = {
+    /**
+     * Converts a url string to an object using url.parse.
+     * @param  {string} oldUrl  A url string
+     * @return {object}         A url object with the query property formatted as an object of parameter keyed arrays.
+     */
+    createUrlObject: function(oldUrl) {
+      var urlObj = url.parse(oldUrl);
+      var query = mapValues(
+        querystring.parse(urlObj.query),
+        function(value){
+          return Array.isArray(value) ? value : value.split(' ');
+        }
+      );
+
+      return Object.assign({}, urlObj, {query: query});
+    },
+
+    /**
+     * Adds query string values to a url
+     * @param   {string} oldUrl                   A url string
+     * @param   {string} param                    A property key
+     * @param   {string, number, array} value     Values to add.
+     * @return  {string}                          A url string.
+     */
+    add: function(oldUrl, param, value) {
+      var urlObj = this.createUrlObject(oldUrl); 
+      var parsedValue = [].concat(parseValue(value));
+
+      if (typeof(parsedValue) === 'undefined') {
+        return oldUrl;
+      }
+
+      urlObj.query[param] = urlObj.query.hasOwnProperty(param) 
+        ? union(urlObj.query[param], parsedValue)
+        : parsedValue;
+            
+      return stringify(urlObj);
+    },
+
+    /**
+     * Removes query string values from a url
+     * @param   {string} oldUrl                   A url string
+     * @param   {string} param                    A property key
+     * @param   {string, number, array} value     Values to remove.
+     * @return  {string}                          A url string.
+     */
+    remove: function(oldUrl, param, value) {
+      var urlObj = this.createUrlObject(oldUrl);
+      var parsedValue = parseValue(value);
+      var newValue = [];
+
+      // Don't do anything if this param doesn't exist.
+      if (!urlObj.query.hasOwnProperty(param)) {
+        return oldUrl;
+      }
+
+      newValue = difference(urlObj.query[param], [].concat(parsedValue));
+
+      if (typeof(value) === 'undefined' || newValue.length === 0) {
+        delete urlObj.query[param];
+      } else {
+        urlObj.query[param] = newValue;
+      }
+
+      return stringify(urlObj);     
+    },
+
+    /**
+     * Sets query string values on a url
+     * @param   {string} oldUrl                   A url string
+     * @param   {string} param                    A property key
+     * @param   {string, number, array} value     Values to set.
+     * @return  {string}                          A url string.
+     */
+    set: function(oldUrl, param, value) {
+      var urlObj = this.createUrlObject(oldUrl); 
+      var parsedValue = parseValue(value);
+      
+      urlObj.query[param] = parsedValue;
+      
+      return stringify(urlObj);
+    }
+  };
+  
+  return pub;
+};
+
+module.exports = urlParams();
---
+++ test/index-test.js
@@ -0,0 +1,82 @@
+// test/index-test.js
+var expect = require('chai').expect
+
+describe('urlParams', function() {
+  var urlParams = require('../index');
+  
+  describe('Adds', function() {
+    it('4 to foo', function() {
+      expect(urlParams.add('http://www.example.com?foo=2+3', 'foo', 4))
+       .to.equal('http://www.example.com/?foo=2+3+4');
+    });
+
+    it('4 and 5 to foo', function() {
+      expect(urlParams.add('http://www.example.com?foo=2+3', 'foo', [4, 5]))
+       .to.equal('http://www.example.com/?foo=2+3+4+5');
+    });
+
+    it('Only unique values', function() {
+      expect(urlParams.add('http://www.example.com?foo=2+3', 'foo', '3'))
+       .to.equal('http://www.example.com/?foo=2+3');
+    });
+
+    it('Numbers as strings', function() {
+      expect(urlParams.add('http://www.example.com?foo=2+3', 'foo', 3))
+       .to.equal('http://www.example.com/?foo=2+3');
+    });
+
+    it('Foo and a new value', function() {
+      expect(urlParams.add('http://www.example.com', 'foo', 3))
+       .to.equal('http://www.example.com/?foo=3');
+    });
+
+    it('A boolean value', function() {
+      expect(urlParams.add('http://www.example.com', 'foo'))
+       .to.equal('http://www.example.com/?foo=true');
+    });
+  });
+
+  describe('Removes', function() {
+    it('3 from foo', function() {
+      expect(urlParams.remove('http://www.example.com?foo=2+3', 'foo', 3))
+        .to.equal('http://www.example.com/?foo=2');
+    });
+
+    it('2 and 3 from foo', function() {
+      expect(urlParams.remove('http://www.example.com?foo=2+3+4', 'foo', [2, 3]))
+        .to.equal('http://www.example.com/?foo=4');
+    });
+
+    it('Foo altogether', function() {
+      expect(urlParams.remove('http://www.example.com?foo=2+3', 'foo'))
+        .to.equal('http://www.example.com/');
+    });
+
+    it('Removes foo if it contains no values', function() {
+      expect(urlParams.remove('http://www.example.com?foo=2', 'foo', 2))
+        .to.equal('http://www.example.com/');
+    });
+
+    it('Nothing if the param doesn\'t exist.', function() {
+      expect(urlParams.remove('http://www.example.com?foo=2', 'bar', 2))
+        .to.equal('http://www.example.com?foo=2');
+    });
+  });
+
+  describe('Set', function() {
+    it('Foo to 3', function() {
+      expect(urlParams.set('http://www.example.com?foo=2+3&bar=baz', 'foo', 3))
+        .to.equal('http://www.example.com/?foo=3&bar=baz');
+    });
+
+    it('Foo to true', function() {
+      expect(urlParams.set('http://www.example.com?foo=2+3', 'foo'))
+        .to.equal('http://www.example.com/?foo=true');
+    });
+
+    it('Foo to 3+4', function() {
+      expect(urlParams.set('http://www.example.com?foo=2', 'foo', [3, 4]))
+        .to.equal('http://www.example.com/?foo=3+4');
+    });
+  });
+});
